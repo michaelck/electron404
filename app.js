3260fcc22a7f10ea3838484e37de424f355bce73
Michael <kappeler.michael@gmail.com>
2016-06-04 15:08:47 -0400

    Remove semicolons

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const ipc = require('electron').ipcRenderer
 
 const selectDirBtn = document.getElementsByClassName('select-directory')
 
-const pressPadBtn = document.getElementsByClassName('pad');
+const pressPadBtn = document.getElementsByClassName('pad')
 
 for (var i = 0; i < selectDirBtn.length; i++) {
-  (function (){
-    var loc = i;
+  ;(function (){
+    var loc = i
     selectDirBtn[i].addEventListener('click', applyit, false)
       function applyit(){
         ipc.send('open-file-dialog', loc)
       }
-    }());
+    }())
 }
 
 ipc.on('selected-directory', function (event, obj) {
@@ -21,17 +21,17 @@ ipc.on('selected-directory', function (event, obj) {
 
 //Start & stop samples by clicking pads
 for (var i = 0; i < pressPadBtn.length; i++) {
-  (function (){
-    var currentPad = i;
+  ;(function (){
+    var currentPad = i
     pressPadBtn[i].addEventListener('click', function (event) {
       if(!this.classList.contains('playing')){
-          document.getElementsByTagName('audio')[currentPad].currentTime = 0;
-          document.getElementsByTagName('audio')[currentPad].play();
-          this.classList.add('playing');
+          document.getElementsByTagName('audio')[currentPad].currentTime = 0
+          document.getElementsByTagName('audio')[currentPad].play()
+          this.classList.add('playing')
       } else {
-        document.getElementsByTagName('audio')[currentPad].pause();
-        this.classList.remove('playing');
+        document.getElementsByTagName('audio')[currentPad].pause()
+        this.classList.remove('playing')
       }
     })
-  }());
+  }())
 }
